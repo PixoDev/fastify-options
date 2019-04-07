71dab5db850c3668b5b31c7897089beca8da0231
Roberto <robertogarciaaragon@gmail.com>
2019-04-07 11:15:23 +0200

    http2 reproduction

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter,NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	/* 
+	In the fastify docs https://github.com/fastify/fastify/blob/master/docs/HTTP2.md appears the
+	http2 option which you can pass through fastify to enable it, it also requires https config
+
+	If you run npm run start:dev you will see a type error:
+	Type '{ http2: boolean; }' has no properties in common with type 'ServerOptions'.
+
+
+	Http2 options exists in fastify.ServerOptionsAsHttp interface
+	
+	*/
+	const opt = {
+		http2: true
+	}
+
+	const app = await NestFactory.create<NestFastifyApplication>(
+    	AppModule,
+    	new FastifyAdapter(opt),
+  	);
+	
+	await app.listen(3000);
 }
 bootstrap();
